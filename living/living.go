0692947a4b59116d246129ce6387823f6fefeda0
Stodgy <80104967+Studgi@users.noreply.github.com>
2024-08-02 03:55:50 +0300

    add value on runtime

--- living/living.go
+++ living/living.go
@@ -41,6 +41,8 @@ type Living struct {
 
 	h  atomic.Value[Handler]
 	mc *entity.MovementComputer
+
+	data atomic.Value
 }
 
 // NewLivingEntity creates a new entity based on the data provided.
@@ -459,4 +461,32 @@ func (e *Living) viewers() []world.Viewer {
 // Handler returns the Handler of the entity.
 func (e *Living) Handler() Handler {
 	return e.h.Load()
+}
+
+// Stores value at runtime. To store it even after server restarts, you'll need to encode/decode NBT.
+func (e *Living) WithValue(key string, value any) {
+	var newData map[string]interface{}
+
+	currentData := e.data.Load()
+	if currentData == nil {
+		newData = make(map[string]interface{})
+	} else {
+		newData = currentData.(map[string]interface{})
+	}
+
+	newData[key] = value
+	e.data.Store(newData)
+}
+
+// retrieves value at runtime. To store it even after server restarts, you'll need to encode/decode NBT. Returns the value and whether it exists.
+func (e *Living) Value(key string) (any, bool) {
+	currentData := e.data.Load()
+	if currentData == nil {
+		return nil, false
+	}
+
+	newData := currentData.(map[string]interface{})
+	value, exists := newData[key]
+
+	return value, exists
 }
